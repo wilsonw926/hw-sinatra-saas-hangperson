eefb5914ff3bcc96cf4a0f678b1ea15706a22815
Wilson Wang <wilsonw926@gmail.com>
2019-09-24 23:51:17 -0700

    Ready for Heroku

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,55 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_reader :word
+  attr_reader :guesses
+  attr_reader :wrong_guesses
+  attr_reader :word_with_guesses
+
+  GUESS_LIMIT = 7
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = '-' * word.length 
+  end
+
+  # processes a guess and modifies the instance variables wrong_guesses and guesses accordingly
+  def guess(letter)
+    if letter == nil || !letter.match(/^[[:alpha:]]$/)
+      raise ArgumentError.new()
+    end
+    guess = letter.downcase
+    # if guess is new
+    if !@guesses.include?(guess) && !@wrong_guesses.include?(guess)
+      # correct guess
+      if @word.downcase.include?(guess)
+        @guesses << guess
+        arr = (0 ... @word.length).find_all { |i| @word[i,1] == letter }
+        arr.each do |i|
+          @word_with_guesses[i] = guess
+        end
+      # incorrect guess  
+      else
+        @wrong_guesses << guess
+      end
+      return true
+    # existing guessed letter
+    else
+      return false
+    end
+  end
+
+  # returns one of the symbols :win, :lose, or :play depending on the current game state
+  def check_win_or_lose
+    if @wrong_guesses.length >= GUESS_LIMIT
+      :lose
+    elsif !(@wrong_guesses.length >= GUESS_LIMIT) && !@word_with_guesses.include?('-')
+      :win
+    else
+      :play
+    end
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
